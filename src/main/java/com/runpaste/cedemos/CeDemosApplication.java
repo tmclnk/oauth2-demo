1d1b2511d62e585c51ffb54b32024cc1e597c817
Tom McLaughlin <tmclaughlin@dmsi.com>
2022-11-07 10:11:27 -0600

    Initial parent project

---
+++ src/main/java/com/runpaste/cedemos/CeDemosApplication.java
@@ -0,0 +1,13 @@
+package com.runpaste.cedemos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CeDemosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CeDemosApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/runpaste/cedemos/CeDemosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.runpaste.cedemos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CeDemosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
