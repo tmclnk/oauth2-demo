2da721b9d10ddd3d88cf9449cf9e68963d03d06a
Tom McLaughlin <tmclaughlin@dmsi.com>
2022-11-07 11:36:11 -0600

    Rename 'person' to 'user'

--- ce-rs-user-profile/src/main/java/com/example/rs/profile/user/UserController.java
+++ ce-rs-user-profile/src/main/java/com/example/rs/profile/user/UserController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class PersonController {
+public class UserController {
 
     @GetMapping("/user/{id}")
     public User get(@PathVariable Long id) {
