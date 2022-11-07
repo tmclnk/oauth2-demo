e716899d675ba10071f671ca1685c089ba16c25f
Tom McLaughlin <tmclaughlin@dmsi.com>
2022-11-07 14:23:10 -0600

    Rename UserProfileApplication

--- ce-rs-user-profile/src/main/java/com/example/rs/profile/UserProfileApplication.java
+++ ce-rs-user-profile/src/main/java/com/example/rs/profile/UserProfileApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CeRsUserProfileApplication {
+public class UserProfileApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CeRsUserProfileApplication.class, args);
+		SpringApplication.run(UserProfileApplication.class, args);
 	}
 
 }
