07672cde30dd8300322edf64acfbef9c4df83bc7
Tom McLaughlin <tmclaughlin@dmsi.com>
2022-11-08 17:42:49 -0600

    Use separate properties class

---
+++ ce-idp/src/main/java/com/example/idp/cloudentity/CloudEntityProperties.java
@@ -0,0 +1,27 @@
+package com.example.idp.cloudentity;
+
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import javax.validation.constraints.NotBlank;
+import java.net.URI;
+import java.util.Map;
+
+@Validated
+@ConfigurationProperties(prefix = "cloudentity")
+public class CloudEntityProperties {
+    @Setter
+    @NotBlank
+    String tenantId;
+
+    public URI acceptURI(String loginId) {
+        return UriComponentsBuilder.newInstance()
+                .scheme("https")
+                .host("{tenant-id}.us.authz.cloudentity.io")
+                .pathSegment("api", "system", "{tenant-id}", "logins", "{login-id}", "accept")
+                .buildAndExpand(Map.of("tenant-id", this.tenantId, "login-id", loginId))
+                .toUri();
+    }
+}
