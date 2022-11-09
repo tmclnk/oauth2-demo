526c1b7998ad936b4024a72a4c5244cfb86f5b17
Tom McLaughlin <tmclaughlin@dmsi.com>
2022-11-09 16:38:35 -0600

    Explain AcceptResponse

--- ce-idp/src/main/java/com/example/idp/cloudentity/AcceptResponse.java
+++ ce-idp/src/main/java/com/example/idp/cloudentity/AcceptResponse.java
@@ -5,6 +5,9 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+/**
+ * The Response from a call to "/accept". Contains a URL to which we should redirect the user.
+ */
 @Getter
 @Setter
 @NoArgsConstructor
