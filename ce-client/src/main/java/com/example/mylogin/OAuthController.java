bb6cf04e31442e32ec04c51dc1ca99b0950e4c8e
Tom McLaughlin <tmclaughlin@dmsi.com>
2022-11-09 20:39:38 -0600

    Fix oauthcontroller view issues

--- ce-client/src/main/java/com/example/mylogin/OAuthController.java
+++ ce-client/src/main/java/com/example/mylogin/OAuthController.java
@@ -1,32 +1,43 @@
 package com.example.mylogin;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.view.RedirectView;
+import reactor.core.publisher.Mono;
+
+import java.net.URI;
 
 /**
  * Just dump auth data as JSON.
  */
 @RestController
+@Slf4j
 public class OAuthController {
     @GetMapping("/oidc")
-    public OidcUser getOidcUserPrincipal(@AuthenticationPrincipal OidcUser principal) {
-        return principal;
+    public Mono<OidcUser> getOidcUserPrincipal(@AuthenticationPrincipal OidcUser principal) {
+        return Mono.just(principal);
     }
 
     @GetMapping("/token")
-    public OAuth2AuthenticationToken getToken(OAuth2AuthenticationToken token) {
-        return token;
+    public Mono<OAuth2AuthenticationToken> getToken(OAuth2AuthenticationToken token) {
+        return Mono.just(token);
+    }
+
+    @GetMapping("/oauth2user")
+    public Mono<OAuth2User> index(@AuthenticationPrincipal OAuth2User principal) {
+        return Mono.just(principal);
     }
 
     @GetMapping("/")
-    public RedirectView index(@AuthenticationPrincipal OAuth2User principal) {
-        return new RedirectView("/person");
-//        return principal;
+    public void index(ServerHttpResponse response) {
+        response.setStatusCode(HttpStatus.FOUND);
+        response.getHeaders().setLocation(URI.create("/person"));
     }
 
 }
