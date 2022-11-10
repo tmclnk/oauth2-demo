6b1fe6d12baa4cf69bbd1548b4c21449a4a7c9ed
Tom McLaughlin <tmclaughlin@dmsi.com>
2022-11-09 20:32:44 -0600

    Use EnableWebFluxSecurity, remove EnableWebMvc

--- ce-client/src/main/java/com/example/mylogin/ClientApplication.java
+++ ce-client/src/main/java/com/example/mylogin/ClientApplication.java
@@ -2,12 +2,10 @@ package com.example.mylogin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 
 @SpringBootApplication
-@EnableWebMvc
-@EnableWebSecurity
+@EnableWebFluxSecurity
 public class ClientApplication {
 
     public static void main(String[] args) {
