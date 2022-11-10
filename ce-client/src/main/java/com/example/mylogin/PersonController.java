ce63fbaaab2ab72710026b34dd9ab9308cce4907
Tom McLaughlin <tmclaughlin@dmsi.com>
2022-11-09 20:44:54 -0600

    Reactive call to resource server

--- ce-client/src/main/java/com/example/mylogin/PersonController.java
+++ ce-client/src/main/java/com/example/mylogin/PersonController.java
@@ -5,13 +5,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
 import org.springframework.security.oauth2.client.annotation.RegisteredOAuth2AuthorizedClient;
-import org.springframework.security.oauth2.client.web.reactive.function.client.ServletOAuth2AuthorizedClientExchangeFilterFunction;
+import org.springframework.security.oauth2.client.web.reactive.function.client.ServerOAuth2AuthorizedClientExchangeFilterFunction;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
 
 import java.util.HashMap;
-import java.util.Map;
 
 @RestController
 @Slf4j
@@ -25,20 +25,15 @@ public class PersonController {
     }
 
     @GetMapping("/person")
-    public Map getPerson(@RegisteredOAuth2AuthorizedClient("cloudentity") OAuth2AuthorizedClient authorizedClient) {
+    public Mono<HashMap> getPerson(@RegisteredOAuth2AuthorizedClient("cloudentity") OAuth2AuthorizedClient authorizedClient) {
         log.trace("Making person call with {}...", authorizedClient.getAccessToken().getTokenValue());
         var result = oauthWebClient
                 .get()
                 .uri("https://spring.users.runpaste.com/users/123")
-                .attributes(ServletOAuth2AuthorizedClientExchangeFilterFunction.oauth2AuthorizedClient(authorizedClient))
+                .attributes(ServerOAuth2AuthorizedClientExchangeFilterFunction.oauth2AuthorizedClient(authorizedClient))
                 .retrieve()
                 .onStatus(HttpStatus::isError, clientResponse -> clientResponse.bodyToMono(String.class).map(Exception::new))
-                .bodyToMono(HashMap.class)
-                .block();
-
-        Map<String, Object> wrapper = new HashMap<>();
-        wrapper.put("hello", "world");
-        wrapper.put("resourceServerResponse", result);
-        return wrapper;
+                .bodyToMono(HashMap.class);
+        return result;
     }
 }
